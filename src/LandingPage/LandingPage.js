08f704002d65e2460c8b11ee4d80495d770aed19
dxu5 <derek_xu@brown.edu>
2020-06-20 15:57:12 -0700

    created logo and implemented it

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,9 +1,12 @@
 import React from "react";
 import TopNav from "./TopNav/TopNav";
+import logo from "../assests/logo.png";
+import styles from "./LandingPage.module.css";
 function LandingPage() {
   return (
     <div>
       <TopNav />
+      <img src={logo} className={styles.logo} alt="delp logo" />
     </div>
   );
 }

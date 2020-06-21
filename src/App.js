026ac8d0fd708adccfdcd51e9e18fa087ba070a4
dxu5 <derek_xu@brown.edu>
2020-06-21 10:15:51 -0700

    added react router and search

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import LandingPage from "./LandingPage/LandingPage";
+import Search from "./Search/Search";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Switch>
+      <Route path="/search" component={Search} />
+      <Route path="/" component={LandingPage} />
+    </Switch>
+  );
 }
 
 export default App;

3bbb54f1625365a6cc15e2976d364272fe578bf0
dxu5 <derek_xu@brown.edu>
2020-06-24 10:32:58 -0700

    created search template

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,12 +1,16 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 import SubNav from "../NavBar/SubNav/SubNav";
+import SearchResultsSummary from "./SearchResultsSummary/SearchResultsSummary";
+import SearchResults from "./SearchResults/SearchResults";
 
 function Search() {
   return (
     <div>
       <NavBar />
       <SubNav />
+      <SearchResultsSummary />
+      <SearchResults />
     </div>
   );
 }
